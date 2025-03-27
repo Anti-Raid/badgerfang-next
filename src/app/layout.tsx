508a6d2da470e3772c45da2cdfbb7b5451690d8d
Miya25 <foenx0315@gmail.com>
2025-03-27 10:38:00 +0530

    feat(fix): stuff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,14 @@ export const metadata: Metadata = {
     },
     description: `${description}`,
     siteName: `${title}`,
+    images: [
+      {
+        url: "/opengraph-image.jpg",
+        width: 1200,
+        height: 630,
+        alt: "AntiRaid by Purrquinox",
+      },
+    ],
   },
   twitter: {
     card: "summary_large_image",
@@ -70,4 +78,3 @@ export default function RootLayout({
     </html>
   )
 }
-
