29e9b0462ed0922a37bd29640ed3850dda75c302
Select <66577268+selectdev@users.noreply.github.com>
2025-03-26 23:50:17 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ export const metadata: Metadata = {
     },
     description: `${description}`,
     siteName: `${title}`,
+    image: '/logo.webp'
   },
   twitter: {
     card: "summary_large_image",
