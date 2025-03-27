04e651a0e1a62f1f9e980e5c44caddf7e21ad88d
Rootspring <cheesycod@outlook.com>
2025-03-27 00:36:15 -0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
   },
   description: `${description}`,
   openGraph: {
-    type: "website",
+    //type: "website",
     //locale: "en_US",
     url: "https://antiraid.xyz",
     title: `${title} - ${description}`,
@@ -50,9 +50,6 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://antiraid.xyz" />
-        <meta property="og:title" content={title} />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Borel&display=swap" rel="stylesheet" />
