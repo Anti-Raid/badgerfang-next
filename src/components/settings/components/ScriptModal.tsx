b0bba939f3b977dd0ae86522093d99a62bbde032
Miya25 <foenx0315@gmail.com>
2025-03-28 21:44:43 +0530

    feat(fix): IDE

--- src/components/settings/components/ScriptModal.tsx
+++ src/components/settings/components/ScriptModal.tsx
@@ -1,8 +1,8 @@
-"use client"
+"use client";
 
-import React, { useState } from "react"
-import { ScriptIDE } from "@/components/ide/ide"
-import { motion, AnimatePresence } from "framer-motion"
+import React from "react";
+import { ScriptIDE } from "@/components/ide/ide";
+import { motion, AnimatePresence } from "framer-motion";
 import { FiX } from "react-icons/fi";
 
 interface ScriptModalProps {
@@ -12,30 +12,44 @@ interface ScriptModalProps {
   scriptName: string;
 }
 
-export const ScriptModal: React.FC<ScriptModalProps> = ({ isOpen, onClose, scriptContent, scriptName }) => {
+export const ScriptModal: React.FC<ScriptModalProps> = ({
+  isOpen,
+  onClose,
+  scriptContent,
+  scriptName,
+}) => {
   return (
     <AnimatePresence>
       {isOpen && (
         <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
+          initial={{ opacity: 0, scale: 0.9 }}
+          animate={{ opacity: 1, scale: 1 }}
+          exit={{ opacity: 0, scale: 0.9 }}
           className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50"
         >
-          <div className="bg-background border border-primary border-opacity-20  p-6 rounded-lg shadow-lg relative w-full max-w-2xl">
-          <button onClick={onClose} className="absolute top-2 right-2 text-gray-500 hover:text-gray-700">
-              <FiX />
+          <div className="bg-background border border-primary border-opacity-20 h-[700px] w-full max-w-4xl rounded-xl shadow-2xl relative flex flex-col">
+            {/* Close Button */}
+            <button
+              onClick={onClose}
+              className="absolute top-4 right-4 text-gray-400 hover:text-gray-200 transition-all duration-200 p-2 rounded-full bg-gray-700/50 hover:bg-gray-700"
+            >
+              <FiX size={22} />
             </button>
-            <h3 className="text-lg font-medium mb-4">{scriptName}.luau</h3>
-            <ScriptIDE
-              files={[{ name: scriptName, path: scriptName, content: scriptContent, type: 'file' }]}
-              isContentEditable={false}
-              height="450px" 
-              width="100%"
-            />
+
+            {/* Modal Title */}
+            <h3 className="text-xl font-semibold p-4 text-gray-100">{scriptName}.luau</h3>
+
+            <div className="flex-2 overflow-hidden mb-3">
+              <ScriptIDE
+                files={[{ name: scriptName, path: scriptName, content: scriptContent, type: "file" }]}
+                isContentEditable={false}
+                height="100%"
+                width="200%"
+              />
+            </div>
           </div>
         </motion.div>
       )}
     </AnimatePresence>
-  )
-}
+  );
+};
