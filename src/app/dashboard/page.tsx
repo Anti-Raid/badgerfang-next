e6091db5421a885a5de21f7f9c2813dbb66a1079
Miya25 <foenx0315@gmail.com>
2025-03-29 10:17:45 +0530

    feat(fix): build errors

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,16 @@
-import { Suspense } from 'react';
-import SearchParamsHandler from '@/components/dashboard/SearchParamsHandler';
+'use client';
+
+import { useSearchParams, useRouter } from 'next/navigation';
+import { useEffect, useState, Suspense } from 'react';
+import Servers from '@/components/dashboard/all-servers';
 
 export default function Dashboard() {
+    const router = useRouter();
+    const [key, setKey] = useState(0);
+
     return (
-        <Suspense fallback={<div>Loading...</div>}>
             <div className="min-h-screen">
-                <SearchParamsHandler />
+                <Servers key={key} />
             </div>
-        </Suspense>
     );
 }
