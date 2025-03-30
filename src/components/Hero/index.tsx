5f3c777a9ea259c7861586a5d153faf47d827c8a
Miya25 <foenx0315@gmail.com>
2025-03-30 11:35:50 +0530

    feat(fix) things

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -5,6 +5,7 @@ import type { StatisticsData } from '@/types/home/StatisticsData';
 import { TemplateCarousel } from './scriptCarosel';
 import { Primary, Secondary } from '../ui/Buttons';
 import { GoArrowUpRight } from 'react-icons/go';
+import { ReviewsCarousel } from './reviewCarosel';
 
 const Hero = () => {
 	const [serverCount, setServerCount] = useState(0);
@@ -127,6 +128,7 @@ const Hero = () => {
 					</div>
 				</main>
 				<TemplateCarousel />
+				<ReviewsCarousel />
 			</section>
 		</>
 	);
