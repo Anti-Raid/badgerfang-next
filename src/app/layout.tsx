bc2a8b50d6543aba97d2570cdbb3646df9d63416
Miya25 <foenx0315@gmail.com>
2025-04-13 21:16:54 +0530

    Feat(add): pwa n og img

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,8 +22,8 @@ export const metadata: Metadata = {
 		images: [
 			{
 				url: 'https://antiraid.xyz/og_image.webp',
-				width: 630,
-				height: 630,
+				width: 480,
+				height: 480,
 				alt: 'AntiRaid by Purrquinox'
 			}
 		]
