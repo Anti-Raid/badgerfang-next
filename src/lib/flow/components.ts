931d48a2a0c08b162e1d0c669d23d47a6c355c2f
Sanandan Sashikumar <sashiks@rose-hulman.edu>
2025-07-10 17:58:21 +0530

    add deleteedge as well

--- src/lib/flow/components.ts
+++ src/lib/flow/components.ts
@@ -1,6 +1,9 @@
+import DeleteEdge from "@/components/flow/DeleteEdge"
 import SetVariable from "@/components/flow/SetVariable"
 
 export const nodeTypes = {
     set_variable: SetVariable,
 }
-export const edgeTypes = {}
+export const edgeTypes = {
+    delete_button: DeleteEdge,
+}
