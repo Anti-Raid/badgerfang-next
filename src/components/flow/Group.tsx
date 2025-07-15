fdbb886edd48fe20b1b6ac9688d24c52eaa7e1c2
Sanandan Sashikumar <sashiks@rose-hulman.edu>
2025-07-15 10:57:49 +0530

    more group fixes

--- src/components/flow/Group.tsx
+++ src/components/flow/Group.tsx
@@ -4,7 +4,7 @@ import { NodeResizer } from "@xyflow/react";
 export default function Group(props: NodeProps) {
   return (
     <div
-      className="pl-1 pr-1 py-1 shadow-md rounded bg-muted/80 border-2 relative min-w-40 min-h-40 h-full cursor-grab"
+      className="pl-1 pr-1 py-1 shadow-md rounded bg-muted/60 border-2 relative min-w-40 min-h-40 h-full cursor-grab"
       style={{
         borderColor: props.selected
           ? "#3B82F6"
